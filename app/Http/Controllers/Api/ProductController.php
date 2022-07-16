cb4fce5079d25ec0c054dfcae3c1e98910ac396b
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-07-16 12:07:37 -0300

    feat: update create products

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -20,9 +20,19 @@ class ProductController extends Controller
         $products = $this->product->all();
         return response()->json($products);
     }
+    public function show($id){
+        $products = $this->product->find($id);
+        return response()->json($products);
+    }
     public function save(Request $request){
         $data = $request->all();
         $product = $this->product->create($data);
+        return response()->json($product);
+    }
+    public function update(Request $request){
+        $data = $request->all();
+        $product = $this->product->find($data['id']);
+        $product->update($data);
         return response()->json($product);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,12 @@ Route::get("/teste",function (Request $request){
 });
 
 //Products Route
-Route::namespace('App\Http\Controllers\Api')->group(function (){
+Route::namespace('App\Http\Controllers\Api')->prefix('products')->group(function (){
 
-    Route::get("/products",'ProductController@index');
-    Route::post("/products",'ProductController@save');
+    Route::get('/','ProductController@index');
+    Route::get('/{id}','ProductController@show');
+    Route::post('/','ProductController@save');
+    Route::put('/','ProductController@update');
 });
 
 
